816ad0585b6fe1d00dc5a9c2d84b1c1c40ef4199
Victorspy-web <dicksonsmart0711@gmail.com>
2022-02-22 21:58:00 +0000

    Initial blog setup

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
+]
---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+# Create your views here.
+
+
+def post_list(request):
+    posts = Post.published.all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'blog/post/index.html', context)
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',
+                            publish__year=year, publish__month=month, publish__day=day)
+
+    context = {
+        
+    }
+
+    return render(request, 'blog/post/detail.html')
