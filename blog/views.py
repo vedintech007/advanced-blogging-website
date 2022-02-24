49c6c5573120a923b5c9a59525dd49ecfa5eea4b
Victorspy-web <dicksonsmart0711@gmail.com>
2022-02-24 22:15:57 +0000

    Done with pagination

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 # Create your views here.
 
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 12) # number of posts to show per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        #if page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        #if page is out of range deliver the last page of the results
+        posts = paginator.page(paginator.num_pages)
 
     context = {
+        'page': page,
         'posts': posts,
+        'object_list': object_list.count()
     }
 
     return render(request, 'blog/post/index.html', context)
