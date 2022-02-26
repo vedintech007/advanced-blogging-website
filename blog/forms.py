ce743aaf85aeffbb816ac0b8f9540d24963d9460
Victorspy-web <dicksonsmart0711@gmail.com>
2022-02-26 12:23:14 +0000

    Design update

--- blog/forms.py
+++ blog/forms.py
@@ -3,12 +3,19 @@ from .models import Comment
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
+    name = forms.CharField(
+        max_length=25, 
+        label="Your Full Name",
+        widget=forms.TextInput()
+        )
+    email = forms.EmailField(label="Your Email")
+    to = forms.EmailField(label="Reciever Email")
     comments = forms.CharField(
         required=False,
-        widget=forms.Textarea
+        label="Your Comment",
+        widget=forms.Textarea(attrs={
+            'placeholder': 'Please add some comments in the section so the reciever can know what this post is about out of the box. \n\nEg. Hi Mark this post talks about programming in dept, please check it out.'
+        })
     )
 
 
