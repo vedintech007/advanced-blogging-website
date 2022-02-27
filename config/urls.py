4e5cd57414ca269cd901054d6a42f13317c0607f
Victorspy-web <dicksonsmart0711@gmail.com>
2022-02-27 21:28:32 +0000

    Added urls and form file to user app

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog')),
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('users', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
